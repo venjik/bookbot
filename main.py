7a578aaf4e0a42780858c5b605c6168f7a67ce7c
Jason Venjik <venjik0@live.com>
2025-05-12 18:11:13 -0400

    Added project files

---
+++ main.py
@@ -0,0 +1,15 @@
+from stats import get_num_words
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
+
---
+++ stats.py
@@ -0,0 +1,4 @@
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
